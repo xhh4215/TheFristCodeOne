5a47514de40926c98c77fe9978f8beaf92045b01
xhh_git <18734184215@163.com>
2017-09-28 13:53:02 +0800

    第二行代码的第一个程序,通过finish()结束退出activity

---
+++ app/src/main/java/com/example/xiaoheihei/FristActivity.java
@@ -0,0 +1,27 @@
+package com.example.xiaoheihei;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class FristActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frist);
+        button = (Button) findViewById(R.id.buttonid);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //通过finish()方法结束当前的activity
+                finish();
+            }
+        });
+    }
+
+}
