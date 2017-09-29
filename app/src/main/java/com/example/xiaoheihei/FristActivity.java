209df9befda8b36ed3bf2dd898caf83b7e95f7cb
xhh_git <18734184215@163.com>
2017-09-29 09:48:59 +0800

    第二行代码的第一个程序,使用intent在程序之间跳转

--- app/src/main/java/com/example/xiaoheihei/FristActivity.java
+++ app/src/main/java/com/example/xiaoheihei/FristActivity.java
@@ -1,5 +1,6 @@
 package com.example.xiaoheihei;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,7 +21,13 @@ public class FristActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //通过finish()方法结束当前的activity
-                finish();
+//                finish();
+                //显示意图的使用
+//                Intent intent = new Intent(FristActivity.this,MainActivity.class);
+//                startActivity(intent);
+                //使用隐示
+                Intent intent = new Intent("com.example.xiaoheihei.ACTION_START");
+                startActivity(intent);
             }
         });
     }
